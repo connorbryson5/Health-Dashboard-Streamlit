24016321c4a61ebccd11f11492ca23b37f84ee02
connorbryson5 <cdbryson@yahoo.com>
2023-11-11 17:46:52 -0500

    Added plotly graph for testing

--- dashboard.py
+++ dashboard.py
@@ -9,7 +9,7 @@ Created on Sat Nov 11 14:07:21 2023
 
 import streamlit as st
 import pandas as pd
-import plost
+import plotly.express as px
 
 # Page Layout
 
@@ -25,30 +25,64 @@ st.sidebar.header('Indicators of Heart Disease Dashboard')
 # Data
 dat = pd.read_csv("https://github.com/connorbryson5/Heart-Disease-Dashboard-Streamlit-/blob/main/data/heart_2022_no_nans.csv?raw=true")
 
-dat.head()
 
-dat.columns
-
+hist1 = px.histogram(dat , x = 'Sex')
 
 # Row A
 a1, a2, a3 = st.columns(3)
 
+with a1:
+    st.markdown('### Bar chart')
+    st.plotly_chart(hist1, use_container_width=True)
+
+with a2:
+    st.markdown('### Bar chart')
+    st.plotly_chart(hist1, use_container_width=True)
+
+
+with a3:
+    st.markdown('### Bar chart')
+    st.plotly_chart(hist1, use_container_width=True)
+
+
 # Row B
 b1, b2, b3 = st.columns(3)
 
+with b1:
+    st.markdown('### Bar chart')
+    st.plotly_chart(hist1, use_container_width=True)
+
+with b2:
+    st.markdown('### Bar chart')
+    st.plotly_chart(hist1, use_container_width=True)
+
+
+with b3:
+    st.markdown('### Bar chart')
+    st.plotly_chart(hist1, use_container_width=True)
+
+
 
 # Row C
-c1, c2, c3 = st.columns((5,5))
+c1, c2, c3 = st.columns(3)
+
+#Histogram
+
+
+
 
 with c1:
     st.markdown('### Bar chart')
-    plost.bar_chart(dat, 
-                    value = 'HeightInMeters',
-                    color = 'CovidPos')
+    st.plotly_chart(hist1, use_container_width=True)
+
+with c2:
+    st.markdown('### Bar chart')
+    st.plotly_chart(hist1, use_container_width=True)
 
 
-
-
+with c3:
+    st.markdown('### Bar chart')
+    st.plotly_chart(hist1, use_container_width=True)
 
 
 
