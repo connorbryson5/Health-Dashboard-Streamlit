717d9e64f1e691176cf43666b18de6dd77cb4378
connorbryson5 <cdbryson@yahoo.com>
2023-11-11 14:58:12 -0500

    Update 2
    Adding style.css file and updating dashboard

--- dashboard.py
+++ dashboard.py
@@ -9,7 +9,52 @@ Created on Sat Nov 11 14:07:21 2023
 
 import streamlit as st
 import pandas as pd
+import plost
+
+# Page Layout
+
+st.set_page_config(layout = "wide", initial_sidebar_state= "expanded")
+
+with open('style.css') as f:
+    st.markdown(f'<style>{f.read()}</style>' , unsafe_allow_html = True)
+
+
+st.sidebar.header('Indicators of Heart Disease Dashboard')
 
 
 # Data
-dat = pd.read_csv("C:/Users/Connor/Desktop/Intro to Informatics/Projects/Heart-Disease-Dashboard-Streamlit-/data")
+dat = pd.read_csv("C:/Users/Connor/Desktop/Intro to Informatics/Projects/Heart-Disease-Dashboard-Streamlit-/data/heart_2022_no_nans.csv")
+
+dat.head()
+
+dat.columns
+
+
+# Row A
+a1, a2, a3 = st.columns(3)
+
+# Row B
+b1, b2, b3 = st.columns(3)
+
+
+# Row C
+c1, c2, c3 = st.columns((5,5))
+
+with c1:
+    st.markdown('### Bar chart')
+    plost.bar_chart(dat, 
+                    value = 'HeightInMeters',
+                    color = 'CovidPos')
+
+
+
+
+
+
+
+
+
+
+
+
+
