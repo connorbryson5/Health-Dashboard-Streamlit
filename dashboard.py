0d0650e4a2ddfa739a85f700384b22bd28e3a0ba
connorbryson5 <cdbryson@yahoo.com>
2023-11-11 14:15:16 -0500

    Update 1
    Uploading data and creating dashboard Python file

---
+++ dashboard.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov 11 14:07:21 2023
+
+@author: Connor
+"""
+
+# Libraries
+
+import streamlit as st
+import pandas as pd
+
+
+# Data
+dat = pd.read_csv("C:/Users/Connor/Desktop/Intro to Informatics/Projects/Heart-Disease-Dashboard-Streamlit-/data")
